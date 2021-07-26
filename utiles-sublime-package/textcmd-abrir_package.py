14517cdd11307adf7a671948a147be7e8e633fe6
caalma <nds.suscript@gmail.com>
2021-07-26 09:27:48 -0300

    Actualización de estilo.

--- utiles-sublime-package/textcmd-abrir_package.py
+++
@@ -1,9 +0,0 @@
-import sublime
-import sublime_plugin
-from os.path import realpath, dirname
-import webbrowser 
-
-
-class AbrirPackageCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        webbrowser.open(dirname(realpath(__file__)))
